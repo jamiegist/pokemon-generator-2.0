4f4a6d26e156bb01ac1ebd6ddcb333bbceab238f
jamiegist <jamie.gist.7@gmail.com>
2025-11-01 11:09:00 -0400

    basic code for actual generator

--- generator.py
+++ generator.py
@@ -5,7 +5,7 @@ import json
 app = Flask(__name__)
 
 def get_pokemon():
-    url = "0"
+    url = ""
     responose = json.loads(requests.request("GET", url).text)
     pokemon_large = response["preview"][-2]
     pokemon_name = response["pokemon"]
@@ -16,4 +16,4 @@ def index():
     pokemon_pic,pokemon_name = get_pokemon()
     return render_template("index.html", pokemon_pic=pokemon_pic, pokemon_name=pokemon_name)
 
-app.run(host="0.0.0.0", port=80)
+# app.run(host="0.0.0.0", port=80) #
