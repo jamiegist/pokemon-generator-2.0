08df8a4c9638409cc287ba55803775626102a8f5
jamieblueg <gist.j@northeastern.edu>
2025-11-01 10:50:25 -0400

    flask test code successful

---
+++ generator.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import requests
+import json
+
+app = Flask(__name__)
+
+def get_pokemon():
+    url = "0"
+    responose = json.loads(requests.request("GET", url).text)
+    pokemon_large = response["preview"][-2]
+    pokemon_name = response["pokemon"]
+    return pokemon_large, pokemon
+
+@app.route("/")
+def index():
+    pokemon_pic,pokemon_name = get_pokemon()
+    return render_template("index.html", pokemon_pic=pokemon_pic, pokemon_name=pokemon_name)
+
+app.run(host="0.0.0.0", port=80)
--- pokemon.py
+++ pokemon.py
@@ -1,9 +1,9 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+    
 @app.route("/")
 def index():
-    return "This is a Pokemon generator"
+    return "<p>This is a Pokemon generator</p>"
 
 app.run(host="0.0.0.0", port=80)
